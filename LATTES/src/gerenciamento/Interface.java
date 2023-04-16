edeb67d1dfbdc38e7f8097656604abe03d00d6ac
Gustavo <123191793+Gus41@users.noreply.github.com>
2023-04-16 18:56:18 -0300

    Update

--- LATTES/src/gerenciamento/Interface.java
+++ LATTES/src/gerenciamento/Interface.java
@@ -16,10 +16,11 @@ public class Interface {
 	public void execute() {
 		Scanner ler = new Scanner(System.in);
 		int j,flag=0,i,qntPES,Nregistrados,n = -1;
-		String nome;
+		String nome,uni;
 		
 		while ( n != 0 ) {
 			n = menu(ler);
+			ler.nextLine();
 			if ( n == 1 ) {
 				artigos[A] = new Artigo();
 				System.out.println("Diga a quantidade de pesquisadores envolvidos: ");
@@ -85,12 +86,13 @@ public class Interface {
 				for ( i = 0 ; i < qntPES ; i ++) {
 					System.out.println("DIGITE O NOME DO PESQUISADOR");
 					nome = ler.nextLine();
-
+					flag = 0;
 					for( j = 0 ; j < P ; j ++) {
 						if(nome.equals(pesquisadores[j].getNome())) {
-							// a fazer 
+							// COLOCAR OS PESQUISADORES DENTRO DO ARRAY DE PESQUISADORES DO PROJETO
 							System.out.println("====PESQUISADOR ENCONTRADO====");
 							flag = 1;
+							break;
 						}
 						
 					}
@@ -101,12 +103,30 @@ public class Interface {
 				}
 			}else if(n == 3 ) {
 				// LISTAR PESQUISADORES DA MESMA FACULDADE
+				System.out.println("Digite o nome da universidade: ");
+				uni = ler.nextLine();
+				flag = 0;
+				for( i = 0 ; i < P ; i ++ ) {
+					if ( pesquisadores[i].getUniversidade().equals(uni)) {
+						flag = 1;
+						System.out.println("Pesquisador: " + pesquisadores[i].getNome());
+					}
+				}
+				if( flag == 0 ) {
+					System.out.println("Nenhum pesquisador encontrado na universidade " + uni );
+				}
 			}else if(n == 4 ) {
 				//  LISTAR AUTORES DE UM DETERMINADO ARTIGO
 			}else if(n == 5 ) {
 				// LISTAR PESQUISADORES DE UM DETERMINADO PROJETO
 			}else if( n == 6 ) {
 				// LISTAR TODOS OS PROJETOS DE UM DETERMINADO PESQUISADOR 
+			}else if(n == 7) {
+				// LISTAR TODOS OS PESQUISADORES
+				System.out.println("=====PESQUISADORES=====");
+				for ( i = 0 ; i < P ; i ++ ) {
+					System.out.println("Nome: " + pesquisadores[i].getNome());
+				}
 			}
 		}
 		
@@ -115,13 +135,14 @@ public class Interface {
 	private int menu(Scanner ler) {
 		int n;
 		System.out.println("=====MENU=====");
-		System.out.println("0 - SAIR");
-		System.out.println("1 - REGISTRAR ARTIGO");
+		System.out.println("0 - SAIR");//*
+		System.out.println("1 - REGISTRAR ARTIGO");//
 		System.out.println("2 - REGISTRAR PROJETO");
-		System.out.println("3 - LISTAR PESQUISADORES DE UMA MESMA FACULDADE");
+		System.out.println("3 - LISTAR PESQUISADORES DE UMA MESMA FACULDADE");//*
 		System.out.println("4 - LISTAR OS AUTORES DE UM DETERMINADO ARTIGO");
 		System.out.println("5- LISTAR PESQUISADORES DE UM DETERMINADO PROJETO");
 		System.out.println("6 - LISTAR TODOS OS PROJETOS DE UM DETERMINADO PESQUISADOR");
+		System.out.println("7 - LISTAR PESQUISADORES");//*
 
 
 		n = ler.nextInt();
